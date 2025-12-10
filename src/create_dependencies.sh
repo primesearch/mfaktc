b6bbfadaa6bbc324400799068265b865afe92aef
Danny Chia <ixfd64@gmail.com>
2025-12-10 02:24:47 -0800

    update CUDA version in create_dependencies.sh

--- src/create_dependencies.sh
+++ src/create_dependencies.sh
@@ -27,7 +27,6 @@ case "${1}" in
     ;;
 esac
 
-(
 # need to define CUDA runtime version during cpp runs because we ignore
 # system includes.
 
@@ -35,7 +34,8 @@ esac
 # selftest-data-wagstaff.c, depending on whether mfaktc is configured for
 # Mersenne or Wagstaff numbers in params.h. For simplicity's sake, we just add
 # both files.
-cpp -D CUDART_VERSION=6050 -include selftest-data-mersenne.c -include \
+(
+cpp -D CUDART_VERSION=13000 -include selftest-data-mersenne.c -include \
     selftest-data-wagstaff.c -MM mfaktc.c
 echo
 
@@ -43,10 +43,10 @@ for FILE in checkpoint.c output.c parse.c read_config.c sieve.c \
             signal_handler.c timer.c tf_96bit.cu tf_barrett96.cu \
             tf_barrett96_gs.cu gpusieve.cu cuda_utils.cu crc.c
 do
-  cpp -D CUDART_VERSION=6050 -MM ${FILE}
+  cpp -D CUDART_VERSION=13000 -MM ${FILE}
   echo
 done
 
 # special case for 75-bit kernels
-cpp -D CUDART_VERSION=6050 -MM tf_96bit.cu -MT tf_75bit.o
+cpp -D CUDART_VERSION=13000 -MM tf_96bit.cu -MT tf_75bit.o
 ) | sed s@\.o:@\.${OBJ}:@
